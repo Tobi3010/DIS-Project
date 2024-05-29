27c1aae40f243b151bf9931ae7184f80718e6914
Mark Seemann <mark@ploeh.dk>
2024-05-29 22:03:33 +0200

    Enable basic cookie-based login/logout
    As the astute reader will no doubt notice, creating a user profile, or
    logging in, requires no password. You can just claim a user name and log
    in as that user.
    
    This is obviously insecure, but we'll make do with that for demo
    purposes, since the purpose of this code base isn't to enable secure
    authentication, but rather to demonstrate use of databases.

---
+++ FiftyBest/Pages/Auth.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Setoma.CompSci.Dis.FiftyBest.Pages;
+public class AuthNModel : PageModel
+{
+    private readonly ILogger<AuthNModel> _logger;
+
+    public AuthNModel(ILogger<AuthNModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public bool IsAuthenticated => User?.Identity?.IsAuthenticated ?? false;
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost(string userName)
+    {
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(
+                new ClaimsIdentity([new Claim(ClaimTypes.Name, userName)],
+                CookieAuthenticationDefaults.AuthenticationScheme)));
+
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostLogoutAsync()
+    {
+        await HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+
+        return RedirectToPage();
+    }
+}
--- FiftyBest/Program.cs
+++ FiftyBest/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 
 var app = builder.Build();
 
@@ -14,6 +20,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
