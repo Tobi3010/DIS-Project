c27b180690bd31b40b04623a130156318b88ba36
Tobi3010 <tobiasandersen62@gmail.com>
2024-06-02 13:47:13 +0200

    Can filter by country and city(minor quirks)

--- FiftyBest/Data/IDataStore.cs
+++ FiftyBest/Data/IDataStore.cs
@@ -7,7 +7,10 @@ public interface IDataStore
     Task CreateUser(string userName);
     Task<bool> UserExists(string userName);
     Task InsertData(string fromYear);
+    
     List<Restaurant> GetRestaurants(string sqlCmd);
+    List<Country> GetCountries(string sqlCmd);
+    List<City> GetCities(string sqlCmd);
 
     
 }
