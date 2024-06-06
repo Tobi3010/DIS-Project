23b699f05c7e59029d65ad78c54cf29ddc0610f4
Mark Seemann <mark@ploeh.dk>
2024-06-06 13:42:04 +0200

    Change Year and Rank to integers

--- FiftyBest/Models/Restaurant.cs
+++ FiftyBest/Models/Restaurant.cs
@@ -3,12 +3,12 @@ namespace Setoma.CompSci.Dis.FiftyBest.Models;
  public class Restaurant
     {
         public string Name { get; }
-        public string Year { get; }
-        public string Rank { get; }
+        public int Year { get; }
+        public int Rank { get; }
         public string City { get; }
         public Restaurant(int year, int rank, string name, string city){
-            Year = year.ToString();
-            Rank = rank.ToString();
+            Year = year;
+            Rank = rank;
             Name = name;
             City = city;
         }
