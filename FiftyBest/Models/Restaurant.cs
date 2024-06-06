4e8232ce7e7973d29599c1257a2e81920197cf3a
Mark Seemann <mark@ploeh.dk>
2024-06-06 13:39:59 +0200

    Make Restaurant properties read-only
    The compiler doesn't complain, and I also can't see that we need to
    change these values once they're loaded from the database.

--- FiftyBest/Models/Restaurant.cs
+++ FiftyBest/Models/Restaurant.cs
@@ -2,10 +2,10 @@ namespace Setoma.CompSci.Dis.FiftyBest.Models;
 
  public class Restaurant
     {
-        public string Name { get; set; }
-        public string Year { get; set; }
-        public string Rank { get; set; }
-        public string City { get; set; }
+        public string Name { get; }
+        public string Year { get; }
+        public string Rank { get; }
+        public string City { get; }
         public Restaurant(int year, int rank, string name, string city){
             Year = year.ToString();
             Rank = rank.ToString();
