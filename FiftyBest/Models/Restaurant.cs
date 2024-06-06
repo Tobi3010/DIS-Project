2ac72c7ae627331b436abf033cb56fdcd2613946
Mark Seemann <mark@ploeh.dk>
2024-06-06 13:43:55 +0200

    Convert Restaurant model from class to record

--- FiftyBest/Models/Restaurant.cs
+++ FiftyBest/Models/Restaurant.cs
@@ -1,15 +1,3 @@
 namespace Setoma.CompSci.Dis.FiftyBest.Models;
 
- public class Restaurant
-    {
-        public string Name { get; }
-        public int Year { get; }
-        public int Rank { get; }
-        public string City { get; }
-        public Restaurant(int year, int rank, string name, string city){
-            Year = year;
-            Rank = rank;
-            Name = name;
-            City = city;
-        }
-    }
+public sealed record Restaurant(int Year, int Rank, string Name, string City);
